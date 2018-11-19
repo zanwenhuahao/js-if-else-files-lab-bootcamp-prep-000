e76ebcb51b88ba4b2ad0c87265926634406f7842
Xuan He <xuan.tappy.he@gmail.com>
2018-11-19 17:26:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ if (name === "Susan")
 {
   console.log("The name is correct");
 }
+else
+{ 
+  console.log("Expected the name to be Susan");
+}
 
 // Don't worry about this
 module.exports = { name, height
